01c126974913869bae7b617b8f7fafbca751809d
felixfavour <felixfavour0@gmail.com>
2024-09-27 21:52:52 +0100

    chore: improved core web vitals

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,21 @@ export default defineNuxtConfig({
       unsplashAccessKey: process.env.UNSPLASH_ACCESS_KEY
     }
   },
+  app: {
+    head: {
+      title: 'Image Catalog',
+      htmlAttrs: {
+        lang: 'en'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' },
+
+      ],
+    },
+  },
   compatibilityDate: '2024-04-03',
   experimental: {
     renderJsonPayloads: false,
